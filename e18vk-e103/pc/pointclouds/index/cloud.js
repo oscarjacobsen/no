2f3493559c2d51a300aa1989e1f29ef5eb93c4b1
Oscar Jacobsen <67653883+oscarjacobsen@users.noreply.github.com>
2023-12-17 16:34:32 +0100

    123

---
+++ e18vk-e103/pc/pointclouds/index/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 397293167,
+    "boundingBox": {
+        "lx": 103034.48168945313,
+        "ly": 1210908.1985778809,
+        "lz": -137.05657958984376,
+        "ux": 104745.02020263672,
+        "uy": 1212618.7370910645,
+        "uz": 1573.48193359375
+    },
+    "tightBoundingBox": {
+        "lx": 103034.482,
+        "ly": 1210908.199,
+        "lz": -137.05700000000003,
+        "ux": 104745.02,
+        "uy": 1211905.178,
+        "uz": 64.542
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 14.813697814941407,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
